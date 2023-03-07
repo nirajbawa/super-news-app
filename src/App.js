e19c3cb1f5127572d91274b1dc98e48489b1b219
Niraj bava <nirajbava222@gmail.com>
2023-03-07 18:26:28 +0530

    add dynamic theme function

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ export default class App extends Component {
       progress:0
     }
     document.body.style.backgroundColor = this.state.theme==="light"?"white":"#070809";
+    document.querySelector("meta[name='theme-color']").setAttribute("content", this.state.theme==="light"?"#ffffff":"#070809")
 
     this.codes = ["ae","ar","at","au","be","bg","br","ca","ch","cn","co","cu","cz","de","eg","fr","gb","gr","hk","hu","id","ie","il","in","it","jp","kr","lt","lv","ma","mx","my","ng","nl","no","nz","ph","pl","pt","ro","rs","ru","sa","se","sg","si","sk","th","tr","tw","ua","us","ve","za"];
     this.cats= ["business","entertainment","health","science","sports","technology"];
